4d8be150680fa6534cd5405f9c9c680a3b7069ff
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2021-09-21 08:44:54 -0300

    enabled to return object list

--- Api/Controllers/CalculatorController.cs
+++ Api/Controllers/CalculatorController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using CoreShared.ModelsDto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Api.Controllers
 {
@@ -16,23 +18,25 @@ namespace Api.Controllers
 
         [HttpGet]
         [Route("/calculate")]
-        public JsonResult GetResult()
+        public async Task<JsonResult> Calculate()
         {
-            var result = JsonSerializer.Serialize(new ResultDto()
-            {
-                Xcoordinate = Convert.ToDecimal(5.1),
-                Ycoordinate = Convert.ToDecimal(7.22),
-                Zcoordinate = Convert.ToDecimal(0.5232)
+            var result = new List<ResultDto>(){
+                new ResultDto()
+                {
+                    Xcoordinate = Convert.ToDecimal(5.1),
+                    Ycoordinate = Convert.ToDecimal(7.22),
+                    Zcoordinate = Convert.ToDecimal(0.5232)
+                },
+                new ResultDto()
+                {
+                    Xcoordinate = Convert.ToDecimal(7.7),
+                    Ycoordinate = Convert.ToDecimal(9.2),
+                    Zcoordinate = Convert.ToDecimal(28991.321)
+                }
+            };
 
-            });
             return new JsonResult(
-                    new ResultDto()
-            {
-                Xcoordinate = Convert.ToDecimal(5.1),
-                Ycoordinate = Convert.ToDecimal(7.22),
-                Zcoordinate = Convert.ToDecimal(0.5232)
-
-            }
+                    result
                 );
         }
     }
