ab0ff093638195ad5c2bc6dd2ff7ae848bdb0062
Quemuel Alves Nassor <quemuel.nassor@novasingular.com.br>
2021-09-24 23:44:11 -0300

    moved request procedures to component

--- Web/Controllers/CalculatorController.cs
+++ Web/Controllers/CalculatorController.cs
@@ -13,52 +13,17 @@ namespace Web.Controllers
 {
     public class CalculatorController : Controller
     {
-        private readonly IConfiguration _configuration;
-        private readonly string UrlApi;
-
-        public CalculatorController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-            UrlApi = _configuration.GetSection("ApiUrl").Value;
-        }
-
-        [HttpPost]
-        // [Route("/calculate")]
+        [HttpGet]
         public async Task<IActionResult> Calculate()
         {
-            try
-            {
-                //Disable certificate validation
-                HttpClientHandler clientHandler = new HttpClientHandler();
-                clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-                //API request data
-                using (HttpClient cli = new HttpClient(clientHandler))
-                {
-                    //API call
-                    var jsonApiResponse = await cli.GetStringAsync(UrlApi + "/calculate");
-
-                    //Json serializer options
-                    JsonSerializerOptions options = new JsonSerializerOptions()
-                    {
-                        PropertyNameCaseInsensitive = true
-                    };
-
-                    //Deserialize the answer into a corresponding template list 
-                    List<ResultDto> responseDto = JsonSerializer.Deserialize<List<ResultDto>>(jsonApiResponse, options);
-
-                    //Return result
-                    // return new JsonResult(responseDto);
-                    return PartialView("_calculatorResultPartial", responseDto);
-                }
-
-            }
-            catch (Exception error)
-            {
-                throw new Exception("Not was possible connect on API: \"" + error.Message + "\"");
-            }
-
+            return ViewComponent("GetCalculatorResult");
         }
+
+        //[HttpGet]
+        //public async Task<IActionResult> AddJoint()
+        //{
+        //    return ViewComponent("");
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
