6c4363626b1e4a83b0290227063d2b0e2a6b6357
Quemuel Alves Nassor <quemuel.nassor@novasingular.com.br>
2021-09-24 23:43:24 -0300

    implemented input model class with data annotations

---
+++ CoreShared/ModelsDto/CalculatorInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CoreShared.ModelsDto
+{
+    public class CalculatorInput
+    {
+        [Required(ErrorMessage = nameof(Theta) + " is required")]
+        public decimal Theta { get; set; }
+        [Required(ErrorMessage = nameof(DistanceD) + " is required")]
+        public decimal DistanceD { get; set; }
+        [Required(ErrorMessage = nameof(DistanceA) + " is required")]
+        public decimal DistanceA { get; set; }
+        [Required(ErrorMessage = nameof(Alpha) + " is required")]
+        public decimal Alpha { get; set; }
+    }
+}
