9513203b9e706789340750f4f8431d050e2f0674
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2021-09-28 10:51:27 -0300

    added reponse format constants

---
+++ CoreShared/Constants/ResultFormatConsts.cs
@@ -0,0 +1,21 @@
+namespace CoreShared.Constants
+{
+    /// <summary>
+    /// Api response types
+    /// </summary>
+    public enum ResultFormatOptions
+    {
+        /// <summary>
+        /// Verbose response
+        /// </summary>
+        V,
+        /// <summary>
+        /// Simple response
+        /// </summary>
+        R,
+        /// <summary>
+        /// Complete response
+        /// </summary>
+        F
+    }
+}
