232fdff43a4e3b17e340ce630b168feda0d0d4e5
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2021-09-28 10:54:09 -0300

    updated api controller

--- Api/Controllers/CalculatorController.cs
+++ Api/Controllers/CalculatorController.cs
@@ -9,37 +9,31 @@ using System.Threading.Tasks;
 
 namespace Api.Controllers
 {
+
+    [ApiController]
+    [Route("/calculate")]
     public class CalculatorController : ControllerBase
     {
-        public CalculatorController()
+        [HttpGet("/{data}")]
+        public async Task<JsonResult> Calculate([FromQuery] CalculatorInput data)
         {
-
-        }
-
-        [HttpGet]
-        [Route("/calculate")]
-        public async Task<JsonResult> Calculate()
-        {
-            var result = new List<CalculatorResultDto>(){
-                new CalculatorResultDto()
+            try
+            {
+                var result = new CalculatorResultDto()
                 {
-                    Joint = "j1",
                     Xcoordinate = Convert.ToDecimal(5.1),
                     Ycoordinate = Convert.ToDecimal(7.22),
                     Zcoordinate = Convert.ToDecimal(0.5232)
-                },
-                new CalculatorResultDto()
-                {
-                    Joint = "j2",
-                    Xcoordinate = Convert.ToDecimal(7.7),
-                    Ycoordinate = Convert.ToDecimal(9.2),
-                    Zcoordinate = Convert.ToDecimal(28991.321)
-                }
-            };
+                };
 
-            return new JsonResult(
-                    result
-                );
+                return new JsonResult(
+                        result
+                    );
+            }
+            catch (Exception error)
+            {
+                throw new Exception("An error occurred when receiving data: \"" + error.Message + "\"");
+            }
         }
     }
 }
