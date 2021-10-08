924ae678ee848e84b3966801c973aae7dc0ad726
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2021-10-08 07:54:46 -0300

    updated result view request component

--- Web/Views/Shared/Components/GetCalculatorResult/GetCalculatorResultViewComponent.cs
+++ Web/Views/Shared/Components/GetCalculatorResult/GetCalculatorResultViewComponent.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -32,10 +34,12 @@ namespace Web.Views.Shared.Components.GetCalculatorResult
             UrlApi = _configuration.GetSection("ApiUrl").Value + "/calculate";
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(CalculatorInput input)
+        public async Task<IViewComponentResult> InvokeAsync(ApiInput input)
         {
             try
             {
+                List<CalculatorResultDto> responseDto = new List<CalculatorResultDto>();
+
                 //Disable ssl certificate validation (for linux environtments)
                 HttpClientHandler clientHandler = new HttpClientHandler();
                 if (_env.IsDevelopment() && Environment.OSVersion.Platform == PlatformID.Unix)
@@ -46,20 +50,25 @@ namespace Web.Views.Shared.Components.GetCalculatorResult
                 //API request call (with resource auto dispose)
                 using (HttpClient cli = new HttpClient(clientHandler))
                 {
-                    //API request already answered in json string format
-                    var queryString = @Url.ActionLink(null, null, input);
-                    var url = UrlApi + queryString.Substring(queryString.IndexOf("?"));
-
-                    var jsonApiResponse = await cli.GetStringAsync(url);
-
                     //Json serializer options (disable case sensitive deserialization)
                     JsonSerializerOptions options = new JsonSerializerOptions()
                     {
                         PropertyNameCaseInsensitive = true
                     };
 
-                    //Deserialize the answer into a corresponding template list 
-                    CalculatorResultDto responseDto = JsonSerializer.Deserialize<CalculatorResultDto>(jsonApiResponse, options);
+                    //Serialize input model
+                    var jsonInputString = JsonSerializer.Serialize(input);
+                    var content = new StringContent(jsonInputString, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+                    //Post request on API
+                    var jsonApiResponse = await cli.PostAsync(UrlApi, content);
+
+                    //Check if response return SUCCESS
+                    jsonApiResponse.EnsureSuccessStatusCode();
+
+                    //Deserialize response
+                    var responseContent = await jsonApiResponse.Content.ReadAsStringAsync();
+                    responseDto = JsonSerializer.Deserialize<List<CalculatorResultDto>>(responseContent, options);
 
                     //Return result
                     return View(responseDto);
