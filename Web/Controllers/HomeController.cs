ff4baa9c82fcc7bdcf68849b0d0c218436b3cfd4
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2021-10-25 21:38:09 -0300

    updated Api action

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -2,11 +2,16 @@
 using CoreShared.ModelsDto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Web.Models;
 
@@ -14,14 +19,17 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostEnvironment _env;
         private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration _configuration;
         private readonly string UrlApi;
 
         public HomeController(
             IConfiguration configuration,
-            ILogger<HomeController> logger)
+            ILogger<HomeController> logger,
+            IHostEnvironment env)
         {
+            _env = env;
             _logger = logger;
             _configuration = configuration;
             UrlApi = _configuration.GetSection("ApiUrl").Value + "/calculate";
@@ -40,6 +48,53 @@ namespace Web.Controllers
             ViewData["Title"] = "Api Documentation";
 
             ViewData["ApiEndpoint"] = UrlApi;
+
+            var exampleInputData = new ApiInput()
+            {
+                Options = ResultFormatOptions.F.ToString(),
+                Joints = new List<CalculatorInput>(){
+                    new CalculatorInput(theta: 90,distanceD: 4,distanceA: 2,alpha: 0),
+                    new CalculatorInput(theta: 180,distanceD: 0,distanceA: 1,alpha: 180),
+                    new CalculatorInput(theta: 0,distanceD: 2,distanceA: 0,alpha: 0),
+                    new CalculatorInput(theta: 90,distanceD: 0,distanceA: 0,alpha: 0)
+                }
+            };
+
+            //Json serializer options (disable case sensitive deserialization)
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            //Serialize input model
+            var jsonInputString = JsonSerializer.Serialize(exampleInputData, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true});
+            var content = new StringContent(jsonInputString, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+            string response;
+
+            //Disable ssl certificate validation (for linux environtments)
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            if (_env.IsDevelopment() && Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            }
+
+            //API request call (with resource auto dispose)
+            using (HttpClient cli = new HttpClient(clientHandler))
+            {
+                //Post request on API
+                var jsonApiResponse = cli.PostAsync(UrlApi, content).Result;
+
+                //Check if response return SUCCESS
+                jsonApiResponse.EnsureSuccessStatusCode();
+
+                //Deserialize response
+                response = jsonApiResponse.Content.ReadAsStringAsync().Result;
+            }
+            var responseDeserialized = JsonSerializer.Deserialize<ApiOutput>(response, options);
+
+            ViewData["CurlExample"] = "$ curl " + UrlApi + " -X POST -v -H \"Content-type: application/json\" -d \n\n'" + jsonInputString + "'";
+            ViewData["ExampleResponse"] = JsonSerializer.Serialize(responseDeserialized, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true, WriteIndented = true });
 
             return View();
         }
