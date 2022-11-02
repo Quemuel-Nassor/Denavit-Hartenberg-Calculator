5b91d3d3eda90275311d22c15f9b90d6da4819f5
Quemuel Alves Nassor <quemuel.nassor@novasingular.com.br>
2022-11-02 18:24:09 -0300

    added calculated fields to cast angles to radians

---
+++ CoreShared/ModelsDto/Joint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using CoreShared.Constants;
+
+namespace CoreShared.ModelsDto
+{
+    public class Joint
+    {
+        public Joint()
+        {
+        }
+
+        /// <summary>
+        /// Overloaded constructor
+        /// </summary>
+        /// <param name="theta"> Theta angle </param>
+        /// <param name="distanceD"> Distance D </param>
+        /// <param name="distanceA"> Distance A </param>
+        /// <param name="alpha"> Alpha Angle </param>
+        public Joint(double theta, double distanceD, double distanceA, double alpha)
+        {
+            Theta = theta;
+            DistanceD = distanceD;
+            DistanceA = distanceA;
+            Alpha = alpha;
+        }
+
+        [Required(ErrorMessage = nameof(Theta) + " is required")]
+        public double Theta { get; set; }
+        public double ThetaRadians => (Math.PI * Theta) / 180;
+
+        [Required(ErrorMessage = nameof(DistanceD) + " is required")]
+        public double DistanceD { get; set; }
+
+        [Required(ErrorMessage = nameof(DistanceA) + " is required")]
+        public double DistanceA { get; set; }
+
+        [Required(ErrorMessage = nameof(Alpha) + " is required")]
+        public double Alpha { get; set; }
+        public double AlphaRadians => (Math.PI * Alpha) / 180;
+
+    }
+}
---
+++ CoreShared/ModelsDto/Result.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+
+namespace CoreShared.ModelsDto
+{
+    /// <summary>
+    /// Class to return result calculated
+    /// </summary>
+    public class Result
+    {
+        public Joint InputInfo { get; set; }
+
+        public List<List<double>> MatrixAn { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Result()
+        {
+
+        }
+
+        /// <summary>
+        /// Overloaded constructor
+        /// </summary>
+        /// <param name="inputData"> input data </param>
+        /// <param name="matrixAn"> dynamic An matrix </param>
+        public Result(Joint inputData, double[,] matrixAn)
+        {
+            int rows = matrixAn.GetLength(0);
+            int cols = matrixAn.GetLength(1);
+            MatrixAn = new List<List<double>>();
+
+            for (int i = 0; i < rows; i++)
+            {
+                MatrixAn.Add(new List<double>());
+
+                for (int j = 0; j < cols; j++)
+                {
+                    MatrixAn[i].Add((double)matrixAn.GetValue(i, j));
+                }
+            }
+
+            InputInfo = inputData;
+        }
+    }
+}
